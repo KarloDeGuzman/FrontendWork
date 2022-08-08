4893b8dc2ecd335674238c2821ecf6c14d3ba1b5
Karlo De Guzman <karlo.deguzman@nathean.com>
2022-08-08 15:24:34 +0100

    initial commit to Social-proof-section folder, added tailwind css config and set up html

--- Social-proof-section/tailwind.config.js
+++ Social-proof-section/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        veryDarkMagenta: "hsl(300, 43%, 22%)",
+        softPink: "hsl(333, 80%, 67%)",
+        darkGrayishMagenta: "hsl(303, 10%, 53%)",
+        lightGrayishMagenta: "hsl(300, 24%, 96%)",
+      },
+    },
   },
   plugins: [],
 };
